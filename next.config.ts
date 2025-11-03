46ba28747a735133c973e0a6cebe5169fac100e3
عبد السلام <abozbere2001@gmail.com>
2025-11-03 20:54:04 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -9,32 +9,9 @@ const pwaConfig = {
   dest: 'public',
   register: true,
   skipWaiting: true,
-  manifest: {
-    name: 'نبض الملاعب',
-    short_name: 'نبض الملاعب',
-    description: 'عالم كرة القدم بين يديك',
-    start_url: isProd ? `/${repoName}/` : '/',
-    display: 'standalone',
-    scope: isProd ? `/${repoName}/` : '/',
-    background_color: '#ffffff',
-    theme_color: '#000000',
-    icons: [
-      {
-        src: isProd ? `/${repoName}/icons/icon-192x192.png` : '/icons/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: isProd ? `/${repoName}/icons/icon-512x512.png` : '/icons/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
-  },
 };
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   basePath: isProd ? `/${repoName}` : '',
   assetPrefix: isProd ? `/${repoName}/` : '',
