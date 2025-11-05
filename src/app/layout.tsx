829e16ba84cd36fc3119e2e362f4490c48911113
عبد السلام <abozbere2001@gmail.com>
2025-11-05 13:42:20 +0000

    هل اكملت

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout({
           <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no, maximum-scale=1.0" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
+          <meta name="theme-color" media="(prefers-color-scheme: light)" content="#FFFFFF" />
         </head>
         <body className={`${cairo.variable} font-body antialiased h-full`}>
             <ThemeProvider
