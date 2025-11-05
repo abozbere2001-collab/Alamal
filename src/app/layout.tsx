c10594572789f7f4dd7d5f090065e11e86d0810d
عبد السلام <abozbere2001@gmail.com>
2025-11-05 08:16:38 +0000

    هل اكملت 😂

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl" suppressHydrationWarning>
+    <html lang="ar" dir="rtl" suppressHydrationWarning className="h-full">
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no, maximum-scale=1.0" />
           <meta name="theme-color" content="#000000" />
@@ -41,7 +41,7 @@ export default function RootLayout({
           <link rel="apple-touch-icon" href="/Alamal/icons/icon-192x192.png" />
           <link rel="icon" type="image/png" sizes="192x192" href="/Alamal/icons/icon-192x192.png" />
         </head>
-        <body className={`${cairo.variable} font-body antialiased`}>
+        <body className={`${cairo.variable} font-body antialiased h-full`}>
             <ThemeProvider
                 attribute="class"
                 defaultTheme="system"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,10 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
