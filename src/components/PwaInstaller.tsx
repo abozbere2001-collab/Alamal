ff6c7960799927c2de296be68586bdc450502f2b
عبد السلام <abozbere2001@gmail.com>
2025-11-06 08:15:48 +0000

    A web manifest is a JSON text file that provides information about a web

--- src/components/PwaInstaller.tsx
+++ src/components/PwaInstaller.tsx
@@ -9,7 +9,7 @@ export function PwaInstaller() {
       typeof window !== 'undefined' &&
       'serviceWorker' in navigator
     ) {
-      navigator.serviceWorker.register('/service-worker.js') // Path is now root
+      navigator.serviceWorker.register('/Alamal/service-worker.js') // Path is now root
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
